142601107afabb0aae168060e3e830ea7b0f40f0
Karim Benmalek <32803877+BENMALEKarim@users.noreply.github.com>
2024-02-02 16:06:06 +0000

    Update application

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Karim BENMALEK and CoP DevOps:  This is new version of ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION') + ' !. Your password is ' + os.environ.get('PASSWORD') + ' . Please use more secure Password !'
+    return 'Hello Karim BENMALEK and CoP DevOps: From ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION') + ' !'
 #app.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"))
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
