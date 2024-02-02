3a061a4fb690e01fd1e90f05253e798a0a5cd7f9
Karim Benmalek <32803877+BENMALEKarim@users.noreply.github.com>
2024-02-02 18:37:08 +0000

    update service

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello Karim BENMALEK and CoP DevOps: From ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION') + ' !'
+    return 'Hello Karim BENMALEK and CoP DevOps: New version From ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION') + ' !'
 #app.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"))
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
