f435c5f8d69c65ad7045fcccbafeaa27132bea26
BENMALEKarim <fk_benmalek@esi.dz>
2024-02-12 14:47:26 +0100

    separate function

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello Karim BENMALEK and CoP DevOps: From ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION') + ' !. Your token is: ' + os.environ.get('TOKEN')
-#app.run(host=os.environ.get("HOST"), port=os.environ.get("PORT"))
+@app.route('/welcome')
+def welcome():
+    return 'Hello Karim BENMALEK and CoP DevOps: From ' + os.environ.get('APP_NAME') + ' application in ' + os.environ.get('ENVIRONMENT') + ' environment. Current verions is ' + os.environ.get('VERSION')
+
+@app.route('/login')
+def login():
+    return ' Your token is: ' + os.environ.get('TOKEN')
+
+
 port = int(os.environ.get('PORT', 5000))
 app.run(debug=True, host='0.0.0.0', port=port)
